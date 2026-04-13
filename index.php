bba4856758f9cbb66801a589be7f1822bf5ff75e
dina-gif <an3678937@gmail.com>
2026-04-13 12:13:57 +0800

    commit pertamaku

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+$nama = "Dina";
+$umur = 20;
+$tinggi = 170.5;
+$menikah = true;
+$hobi = ["membaca", "berenang", "main game"];
+
+echo "nama saya $nama, umur saya $umur, tinggi saya $tinggi, status saya $menikah, hobi saya $hobi[2]";
+
+?>
