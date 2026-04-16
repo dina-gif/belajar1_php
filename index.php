b0a4247083af0fa2c3a3b7a76be68839d088427a
dina-gif <an3678937@gmail.com>
2026-04-16 08:46:42 +0800

    commit keduaku

--- index.php
+++ index.php
@@ -2,10 +2,40 @@
 
 $nama = "Dina";
 $umur = 20;
-$tinggi = 170.5;
+$tinggi = 147.5;
 $menikah = true;
 $hobi = ["membaca", "berenang", "main game"];
 
 echo "nama saya $nama, umur saya $umur, tinggi saya $tinggi, status saya $menikah, hobi saya $hobi[2]";
+
+echo "<br><br>=============================================================<br><br>";
+
+//operator
+$nilai1 = 5;
+$nilai2 = 8;
+$nilai3 = 10;
+$nilai4 = 20;
+$nilai5 = 13;
+
+$hasil = $nilai1 + $nilai2 - $nilai3 * $nilai4 / $nilai5;
+
+echo "hasil dari $nilai1 + $nilai2 - $nilai3 * $nilai4 / $nilai5 adalah $hasil";
+
+echo "<br><br>=============================================================<br><br>";
+
+//percabangan
+$nilai = 70;
+
+if($nilai >= 85) {
+    echo "grade A";
+} else if($nilai >= 70){
+    echo "grade B";
+}else if($nilai >= 50){
+    echo "grade C";
+}else if($nilai >= 30){
+    echo "grarde D";
+}else{
+    echo "grade E";
+}
 
 ?>
