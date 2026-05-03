ba7ae90d0a1e3375e6ef9a3daaddf15d3a9e49b8
dina-gif <an3678937@gmail.com>
2026-05-03 23:41:59 +0800

    commit keempatku

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    echo "<br><br><a href='materi1.php'>materi 1 : If dan Else</a>";
+    echo "<br><br><a href='materi2.php'>materi 2 : Looping</a>";
+    echo "<br><br><a href='materi3.php'>materi 3 : Function</a>";
+    echo "<br><br><a href='materi4.php'>materi 4 : Array</a>";
+    echo "<br><br><a href='materi5.php'>materi 5 : OOP</a>";
+    echo "<br><br><a href='materi6.php'>materi 6 : Database</a>";
+    echo "<br><br><a href='materi7.php'>materi 7 : Session dan Cookie</a>";
+    echo "<br><br><a href='materi8.php'>materi 8 : File Handling</a>";
+    echo "<br><br><a href='materi9.php'>materi 9 : Error Handling</a>";
+    echo "<br><br><a href='materi10.php'>materi 10 : Security</a>";
+?>
