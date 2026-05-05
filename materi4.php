3b0ebe58555c513e24d9a9fc9cbea72d98bd6c7a
dina-gif <an3678937@gmail.com>
2026-05-05 11:24:16 +0800

    commit kelimaku

--- materi4.php
+++ materi4.php
@@ -15,6 +15,18 @@ if (isset($_POST['kirim'])) {
         echo "data gagal di tambahkan";
     }
 } 
+
+//proses hapus
+if (isset($_GET['hapus'])) {
+    $id = $_GET['hapus'];
+    $sql = "DELETE FROM user WHERE id_user = '$id'";
+    $query = mysqli_query($conn, $sql);
+    if($query){
+        echo "data berhasil dihapus";
+    } else {
+        echo "data tidak berhasil dihapus";
+    }
+}
 ?>
 
 <form method="POST">
@@ -25,3 +37,31 @@ if (isset($_POST['kirim'])) {
     <input type="submit" value="kirim data" name="kirim">
 </form>
 
+//menampilkan data
+
+<table border="1">
+    <tr>
+        <th>id_user</th>
+        <th>username</th>
+        <th>password</th>
+        <th>nama</th>
+        <th>email</th>
+        <th>aksi</th>
+    </tr>
+
+    <?php
+        $sql = "SELECT * FROM user";
+        $query = mysqli_query($conn, $sql);
+        while($row = mysqli_fetch_assoc($query)){
+            echo "<tr>
+            <td>{$row['id_user']}</td>
+            <td>{$row['username']}</td>
+            <td>{$row['password']}</td>
+            <td>{$row['nama']}</td>
+            <td>{$row['email']}</td>
+            <td><a href='materi4.php?hapus={$row['id_user']}'>hapus</a> | <a href=?edit={$row['id_user']}'>edit</a></td>
+            </tr>";
+        }
+    ?>
+
+</table>
